631edba1d694604fa3493dcc9474bc8525ee1db6
Justin Hoelscher <justinh7@vt.edu>
2018-08-24 16:00:08 +0000

    master: Adding new endpoint

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -6,47 +6,58 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.StaticHandler;
+import groovy.util.Eval;
 
 import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApplication extends AbstractVerticle {
 
-  static final String template = "Hello, %s!";
+    static final String template         = "Hello, %s!";
 
-  @Override
-  public void start(Future<Void> future) {
-    // Create a router object.
-    Router router = Router.router(vertx);
+    static final String responseTemplate = "Output: %s";
 
-    router.get("/api/greeting").handler(this::greeting);
-    router.get("/*").handler(StaticHandler.create());
+    @Override
+    public void start(Future<Void> future) {
+        // Create a router object.
+        Router router = Router.router(vertx);
 
-    // Create the HTTP server and pass the "accept" method to the request handler.
-    vertx
-        .createHttpServer()
-        .requestHandler(router::accept)
-        .listen(
-            // Retrieve the port from the configuration, default to 8080.
-            config().getInteger("http.port", 8080), ar -> {
-              if (ar.succeeded()) {
+        router.get("/api/greeting").handler(this::greeting);
+        router.post("/api/evaluator").handler(this::evaluator);
+        router.get("/*").handler(StaticHandler.create());
+
+        // Create the HTTP server and pass the "accept" method to the request handler.
+        vertx.createHttpServer().requestHandler(router::accept).listen(
+        // Retrieve the port from the configuration, default to 8080.
+        config().getInteger("http.port", 8080), ar -> {
+            if (ar.succeeded()) {
                 System.out.println("Server started on port " + ar.result().actualPort());
-              }
-              future.handle(ar.mapEmpty());
-            });
+            }
+            future.handle(ar.mapEmpty());
+        });
 
-  }
-
-  private void greeting(RoutingContext rc) {
-    String name = rc.request().getParam("name");
-    if (name == null) {
-      name = "World";
     }
 
-    JsonObject response = new JsonObject()
-        .put("content", String.format(template, name));
+    private void greeting(RoutingContext rc) {
+        String name = rc.request().getParam("name");
+        if (name == null) {
+            name = "World";
+        }
 
-    rc.response()
-        .putHeader(CONTENT_TYPE, "application/json; charset=utf-8")
-        .end(response.encodePrettily());
-  }
+        JsonObject response = new JsonObject().put("content", String.format(template, name));
+
+        rc.response().putHeader(CONTENT_TYPE, "application/json; charset=utf-8").end(response.encodePrettily());
+    }
+
+    private void evaluator(RoutingContext rc) {
+        rc.request().bodyHandler(bodyHandler -> {
+            final JsonObject body = bodyHandler.toJsonObject();
+            String groovyCode = body.getString("groovyCode");
+            String output = Eval.me(groovyCode).toString();
+            JsonObject response = new JsonObject().put("content", String.format(responseTemplate, output));
+
+            rc.response().putHeader(CONTENT_TYPE, "application/json; charset=utf-8").end(response.encodePrettily());
+        });
+
+
+    }
 }
